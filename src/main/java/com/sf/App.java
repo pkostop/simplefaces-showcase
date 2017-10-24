a1b079641f7daabd6a40177ffabc7fdc04b60a3c
pkostopoulos <pkostopoulos>
2017-10-24 13:15:00 +0300

    first commit

---
+++ src/main/java/com/sf/App.java
@@ -0,0 +1,79 @@
+package com.sf;
+
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	Server server = new Server( 8080 );
+
+        // Setup JMX
+        MBeanContainer mbContainer = new MBeanContainer(
+                ManagementFactory.getPlatformMBeanServer() );
+        server.addBean( mbContainer );
+
+        // The WebAppContext is the entity that controls the environment in
+        // which a web application lives and
+        // breathes. In this example the context path is being set to "/" so it
+        // is suitable for serving root context
+        // requests and then we see it setting the location of the war. A whole
+        // host of other configurations are
+        // available, ranging from configuring to support annotation scanning in
+        // the webapp (through
+        // PlusConfiguration) to choosing where the webapp will unpack itself.
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath( "/" );
+        App.class.getResource("/META-INF/simplefaces-showcase-war.war");
+        File warFile = new File(
+                "C:\\projects\\simplefaces-showcase\\src\\main\\resources\\simplefaces-showcase-war.war" );
+        if (!warFile.exists())
+        {
+            throw new RuntimeException( "Unable to find WAR File: "
+                    + warFile.getAbsolutePath() );
+        }
+        webapp.setWar( warFile.getAbsolutePath() );
+        webapp.setExtractWAR(true);
+
+        // This webapp will use jsps and jstl. We need to enable the
+        // AnnotationConfiguration in order to correctly
+        // set up the jsp container
+        Configuration.ClassList classlist = Configuration.ClassList
+                .setServerDefault( server );
+        classlist.addBefore(
+                "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration" );
+
+        // Set the ContainerIncludeJarPattern so that jetty examines these
+        // container-path jars for tlds, web-fragments etc.
+        // If you omit the jar that contains the jstl .tlds, the jsp engine will
+        // scan for them instead.
+        webapp.setAttribute(
+                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
+
+        // A WebAppContext is a ContextHandler as well so it needs to be set to
+        // the server so it is aware of where to
+        // send the appropriate requests.
+        server.setHandler( webapp );
+
+
+
+        // Start things up!
+        server.start();
+
+        server.dumpStdErr();
+
+        // The use of server.join() the will make the current thread join and
+        // wait until the server is done executing.
+        // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+        server.join();
+    }
+}
